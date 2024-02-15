e7c7abea4933c71d333503eb744349470eac5e37
Ольга Скрябина <ibonish@MacBook-Air.local>
2024-02-15 20:48:41 +0300

    proj title

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    app_title: str
+    app_title: str = 'Сервис для поддержки котиков!'
     database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
     secret: str = 'SECRET'
 
